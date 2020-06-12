1b2c507687fa55fdc70970e3103c5a367c018b57
cli <cli@DE-D-CLI>
2020-06-12 10:47:41 -0700

    Update MfgDataTraceRecord.cs

--- DBHelper/SQLTable/MfgDataTraceRecord.cs
+++ DBHelper/SQLTable/MfgDataTraceRecord.cs
@@ -18,7 +18,7 @@ namespace DBHelper.SQLTable
 
         public static void LogRecord(string theText)
         {
-            string sqlCmd = string.Format("insert into TraceRecord (LogText) values ('{0}')", theText);
+            string sqlCmd = string.Format("insert into TraceRecord (LogText, LogUser, LogComputer) values ('{0}', '{1}', '{2}')", theText, Environment.UserName, Environment.MachineName);
             dbAccess.SetQueryCmd(sqlCmd);
             dbAccess.RunSQLcmd();
         }
