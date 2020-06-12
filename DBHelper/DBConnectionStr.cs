53dd4d522e998d3e34331835ef7e0549b88b40de
cli <cli@DE-D-CLI>
2020-06-12 10:43:37 -0700

    Update DBConnectionStr.cs

--- DBHelper/DBConnectionStr.cs
+++ DBHelper/DBConnectionStr.cs
@@ -9,11 +9,19 @@ namespace DBHelper
     public static class DBConnectionStr
     {
         const string ConnectionStrMfgData = "Data Source=amc-sql01;Initial Catalog=AMC_MfgData;Persist Security Info=True;User ID=autest;Password=autest1234";
-        private static string ConnectionStrMfgDataDev = "Data Source=amc-sql01;Initial Catalog=AMC_MfgDataDev;Persist Security Info=True;User ID=autest;Password=autest1234";
+        public const string ConnectionStrMfgDataDev = "Data Source=amc-sql01;Initial Catalog=AMC_MfgDataDev;Persist Security Info=True;User ID=autest;Password=autest1234";
 
-        const string ConnStrEpicorProd = "Data Source=bitis;Initial Catalog=epicor905;Persist Security Info=True;User ID=e9prog;Password=e9prog";
+        private static string ConnectionStrTestData = "Data Source=amc-sql01;Initial Catalog=AMC_TestData;Persist Security Info=True;User ID=autest;Password=autest1234";
 
-        // Stop developing code for FoxPro
+        private static string ConnectionStrProdmanData = "Data Source=amc-sql01;Initial Catalog=AMC_Prodman;Persist Security Info=True;User ID=cliservice;Password=n3wCopper24";
+
+        public const string ConnectionStrPLMDataLoad = @"Data Source=amc-plm-01;Initial Catalog=DataLoad;Persist Security Info=True;User ID=plmload;Password='=reeWing28'";
+        /*** harlequin - prodmansql / pr0dman5ux ***/
+
+        public const string ConnStrEpicorProd = "Data Source=bitis;Initial Catalog=epicor905;Persist Security Info=True;User ID=e9prog;Password=e9prog";
+        const string ConnStrEpicorTest = "Data Source=bitis;Initial Catalog=epicortest905;Persist Security Info=True;User ID=e9prog;Password=e9prog";
+
+        /******* Stop developing code for FoxPro *******/
         private static string OLEFoxProProd = @"Data Source=\\BOA\Production\Prodman.vfp\DATABASE\{0}.dbf;Provider=VFPOLEDB.1;";
         private static string OLEFoxProDev = @"Data Source=\\BOA\Production\Prodman.dev\DATABASE\{0}.dbf;Provider=VFPOLEDB.1;";
 
@@ -22,19 +30,37 @@ namespace DBHelper
         private static string ODBCFoxProDev3 = ".DBF;SourceType=DBF;Exclusive=No;BackgroundFetch=Yes;Collate=Machine;Null=Yes;Deleted=Yes;";
 
         private static string testStr = @"Driver={Microsoft dBASE Driver (*.dbf)};DriverID=277;Dbq=\\BOA\Production\Prodman.vfp\DATABASE\amplifs.DBC;";
+        /***********************************************/
 
         public static string ConnStrMfgData()
         {
 #if DEBUG
-            return ConnectionStrMfgDataDev;
+            return ConnectionStrMfgData;
+
+            //return ConnectionStrMfgDataDev;
 #else
             return ConnectionStrMfgData;
 #endif
         }
 
+        public static string ConnStrTestData()
+        {
+            return ConnectionStrTestData;
+        }
+
+        public static string ConnStrProdmanData()
+        {
+            return ConnectionStrProdmanData;
+        }
+
         public static string SQLEpicorConnStr()
         {
+#if DEBUG
+            return ConnStrEpicorProd;                           // For AssociateSubassembly test on 1/14/2020
+            //return ConnStrEpicorTest;
+#else
             return ConnStrEpicorProd;
+#endif
         }
 
         /*-------*/
