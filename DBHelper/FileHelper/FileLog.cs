f25abc7c3769d23c61c4fea7b986fdc658c55060
cli <cli@DE-D-CLI>
2020-06-12 10:46:06 -0700

    Create FileLog.cs

---
+++ DBHelper/FileHelper/FileLog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DBHelper.FileHelper
+{
+    public static class FileLog
+    {
+        private static string _PathNFile;
+
+        internal static void SetPathNFile(string path)
+        {
+            _PathNFile = path;
+        }
+        internal static void LogMessage(string aMsg)
+        {
+            string localDateTime = Convert.ToString(DateTime.Now);
+            File.AppendAllText(_PathNFile, string.Format("{0}: {1}\r\n", localDateTime, aMsg));
+        }
+
+    } // class
+}
