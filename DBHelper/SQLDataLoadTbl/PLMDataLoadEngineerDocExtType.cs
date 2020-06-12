307a223248f52d75351e12a8f132140222bf04f4
cli <cli@DE-D-CLI>
2020-06-12 10:46:51 -0700

    Create PLMDataLoadEngineerDocExtType.cs

---
+++ DBHelper/SQLDataLoadTbl/PLMDataLoadEngineerDocExtType.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBHelper.SQLDataLoadTbl
+{
+    public class PLMDataLoadEngineerDocExtType
+    {
+        private AccessSQL dbAccess = new AccessSQL();
+
+        // Class constructor
+        public PLMDataLoadEngineerDocExtType()
+        {
+            dbAccess.SetConnStr(DBConnectionStr.ConnectionStrPLMDataLoad);
+        }
+
+        public string GetDocumentType(string docExt)
+        {
+            string ret = "";
+
+            string sqlCmd = string.Format("select top 1 DocumentType from EngineerDocExtType where FileExtension like '{0}%' order by FileExtension", docExt);
+            dbAccess.SetQueryCmd(sqlCmd);
+            object retVal = dbAccess.GetASingleValue();
+            if (retVal != null)
+            {
+                ret = (string)retVal;
+            }
+
+            return ret;
+        }
+
+    }
+}
