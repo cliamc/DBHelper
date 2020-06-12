5629fd08825577cfa9de6070949f23c85fc966e0
cli <cli@DE-D-CLI>
2020-06-12 10:46:57 -0700

    Create ProdmanBeta.cs

---
+++ DBHelper/SQLDataLoadTbl/ProdmanBeta.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBHelper.SQLDataLoadTbl
+{
+    public class ProdmanBeta
+    {
+        private AccessSQL dbAccess = new AccessSQL();
+
+        public ProdmanBeta()
+        {
+            dbAccess.SetConnStr(DBConnectionStr.ConnStrProdmanData());
+        }
+
+        public DataTable SelectNotes()
+        {
+            string sqlCmd = string.Format("select partnumber, [version], notes, testnotes, fanotes, gennotes from beta");
+            dbAccess.SetQueryCmd(sqlCmd);
+            DataTable retVal = dbAccess.ReadDbData();
+
+            return retVal;
+        }
+
+    } // class
+}
