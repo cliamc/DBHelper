4995df53d6f38f37c195c7f3d03147669452f8de
cli <cli@DE-D-CLI>
2020-06-12 10:46:17 -0700

    Create LineColumn.cs

---
+++ DBHelper/FileHelper/LineColumn.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBHelper.FileHelper
+{
+    public class LineColumn
+    {
+        private string _cValue;
+
+        public string cValue
+        {
+            get { return _cValue.TrimEnd(' '); }
+
+            set { _cValue = value; }
+        }
+
+    } // class
+}
