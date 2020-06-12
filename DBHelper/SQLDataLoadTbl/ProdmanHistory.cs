ca899f59753c4d4ee7a69ed0b5e8873bcc298b34
cli <cli@DE-D-CLI>
2020-06-12 10:47:02 -0700

    Create ProdmanHistory.cs

---
+++ DBHelper/SQLDataLoadTbl/ProdmanHistory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBHelper.SQLDataLoadTbl
+{
+    public class ProdmanHistory
+    {
+        private AccessSQL dbAccess = new AccessSQL();
+
+        public ProdmanHistory()
+        {
+            dbAccess.SetConnStr(DBConnectionStr.ConnStrProdmanData());
+        }
+
+        public DataTable SelectNotes()
+        {
+            string sqlCmd = string.Format("select partnumber, [version], notes, testnotes, fanotes, gennotes from history order by partnumber");
+            dbAccess.SetQueryCmd(sqlCmd);
+            DataTable retVal = dbAccess.ReadDbData();
+
+            return retVal;
+        }
+
+    }
+}
