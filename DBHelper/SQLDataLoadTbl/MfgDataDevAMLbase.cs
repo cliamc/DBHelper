b81de655d5b3cdb98649d3f60979846aec716abd
cli <cli@DE-D-CLI>
2020-06-12 10:46:21 -0700

    Create MfgDataDevAMLbase.cs

---
+++ DBHelper/SQLDataLoadTbl/MfgDataDevAMLbase.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+
+namespace DBHelper.SQLTable
+{
+    public class MfgDataDevAMLbase
+    {
+
+        private AccessSQL dbAccess = new AccessSQL();
+
+        public MfgDataDevAMLbase()
+        {
+            dbAccess.SetConnStr(DBConnectionStr.ConnectionStrMfgDataDev);
+        }
+
+        public DataTable SelectTbl()
+        {
+            string sqlCmd = string.Format("select * from AMLbase");
+            dbAccess.SetQueryCmd(sqlCmd);
+            DataTable dt = dbAccess.ReadDbData();
+
+            return dt;
+        }
+
+    } // class
+}
