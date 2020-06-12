19da2045cf65c0c5d2b5a3a96f0ab950820078c9
cli <cli@DE-D-CLI>
2020-06-12 10:46:34 -0700

    Create PLMDataLoadBOMwithLifeCycle.cs

---
+++ DBHelper/SQLDataLoadTbl/PLMDataLoadBOMwithLifeCycle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBHelper.SQLTable
+{
+    public class PLMDataLoadBOMwithLifeCycle
+    {
+        private AccessSQL dbAccess = new AccessSQL();
+
+        public PLMDataLoadBOMwithLifeCycle()
+        {
+            dbAccess.SetConnStr(DBConnectionStr.ConnectionStrPLMDataLoad);
+        }
+
+        public string GetLifeCycle(string part, string ver)
+        {
+            string ret = "";
+
+            string sqlCmd = string.Format("select top 1 LifeCycle from BOMwithLifeCycle where PartNumber = '{0}' and Revision = '{1}'", part, ver);
+            dbAccess.SetQueryCmd(sqlCmd);
+            object retVal = dbAccess.GetASingleValue();
+            if (retVal != null)
+            {
+                ret = (string)retVal;
+            }
+
+            return ret;
+        }
+
+        public string GetLifeCycle(string part)
+        {
+            string ret = "";
+
+            string sqlCmd = string.Format("select top 1 LifeCycle from BOMwithLifeCycle where AMC_PartNum = '{0}'", part);
+            dbAccess.SetQueryCmd(sqlCmd);
+            object retVal = dbAccess.GetASingleValue();
+            if (retVal != null)
+            {
+                ret = (string)retVal;
+            }
+
+            return ret;
+        }
+
+    } // class
+}
