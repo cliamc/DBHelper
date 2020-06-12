af9a14373b8e0f27372d90aa757a2e34865b2cb7
cli <cli@DE-D-CLI>
2020-06-12 10:47:06 -0700

    Create ProdmanProto.cs

---
+++ DBHelper/SQLDataLoadTbl/ProdmanProto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBHelper.SQLDataLoadTbl
+{
+    public class ProdmanProto
+    {
+        private AccessSQL dbAccess = new AccessSQL();
+
+        public ProdmanProto()
+        {
+            dbAccess.SetConnStr(DBConnectionStr.ConnStrProdmanData());
+        }
+
+        public DataTable SelectNotes()
+        {
+            string sqlCmd = string.Format("select partnumber, [version], notes, testnotes, fanotes, gennotes from proto");
+            dbAccess.SetQueryCmd(sqlCmd);
+            DataTable retVal = dbAccess.ReadDbData();
+
+            return retVal;
+        }
+
+    } // class
+}
