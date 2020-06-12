2ef52ab68431ba5bdba461e4ba5783c0f11f7091
cli <cli@DE-D-CLI>
2020-06-12 10:47:00 -0700

    Create ProdmanCurrent.cs

---
+++ DBHelper/SQLDataLoadTbl/ProdmanCurrent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBHelper.SQLDataLoadTbl
+{
+    public class ProdmanCurrent
+    {
+        private AccessSQL dbAccess = new AccessSQL();
+
+        public ProdmanCurrent()
+        {
+            dbAccess.SetConnStr(DBConnectionStr.ConnStrProdmanData());
+        }
+
+        public DataTable SelectNotes()
+        {
+            string sqlCmd = string.Format("select partnumber, [version], notes, testnotes, fanotes, gennotes from [current]");
+            dbAccess.SetQueryCmd(sqlCmd);
+            DataTable retVal = dbAccess.ReadDbData();
+
+            return retVal;
+        }
+
+    } // class
+}
