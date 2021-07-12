0ab063e87e159c70b2e2e54a74d288d5e47b4505
cli <cli@DE-D-CLI>
2021-07-12 11:02:50 -0700

    Create MfgDataPLDDevice.cs

---
+++ DBHelper/SQLTable/MfgDataPLDDevice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBHelper.SQLTable
+{
+    public class MfgDataPLDDevice
+    {
+        private AccessSQL dbAccess = new AccessSQL();
+
+        public MfgDataPLDDevice()
+        {
+            dbAccess.SetConnStr(DBConnectionStr.ConnStrMfgData());
+        }
+
+        public DataTable GetPLDparams(string amcPN)
+        {
+            string sqlCmd = string.Format("select * from PLDDevice where AMCPartNumber = '{0}'", amcPN);
+            dbAccess.SetQueryCmd(sqlCmd);
+            DataTable retVal = dbAccess.ReadDbData();
+
+            return retVal;
+        }
+
+    } // class
+}
