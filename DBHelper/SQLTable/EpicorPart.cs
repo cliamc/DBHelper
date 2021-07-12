0d37ddea03d1e0c5c744a2f380be8150a63b5f31
cli <cli@DE-D-CLI>
2021-07-12 11:02:28 -0700

    Create EpicorPart.cs

---
+++ DBHelper/SQLTable/EpicorPart.cs
@@ -0,0 +1,47 @@
+﻿using System.Data;
+
+namespace DBHelper.SQLTable
+{
+    public class EpicorPart
+    {
+        private string AMCPartNumber = "";
+
+        private AccessSQL dbAccess = new AccessSQL();
+
+        public EpicorPart()
+        {
+            dbAccess.SetConnStr(DBConnectionStr.SQLEpicorConnStr());
+        }
+
+        //public string GetAMCPartNumber(string pn)
+        //{
+        //    string sqlCmd = string.Format("select Character01 from Part where PartNum = '{0}'", pn);
+        //    dbAccess.SetQueryCmd(sqlCmd);
+        //    object retVal = dbAccess.GetASingleValue();
+
+        //    if (retVal != null)
+        //    {
+        //        this.AMCPartNumber = (string)retVal;
+        //    }
+        //    else
+        //    {
+        //        this.AMCPartNumber = pn;
+        //    }
+
+        //    return this.AMCPartNumber;
+        //}
+
+        public DataTable GetAMCPartNumber(string pn)
+        {
+            string sqlCmd = string.Format("select Character01, Character02, Character03 from Part where PartNum = '{0}'", pn);
+
+            dbAccess.SetQueryCmd(sqlCmd);
+
+            ///DataTable dt = dbAccess.ReadDbData();
+            DataTable dt = dbAccess.LoadDbData();
+
+            return dt;
+        }
+
+    } // class
+}
