c9bff20ae9a55f8203512a0d1cb7f725616774a2
cli <cli@DE-D-CLI>
2024-02-12 08:41:26 -0800

    Create KineticSNTran.cs

---
+++ DBHelper/SQLKinetic/KineticSNTran.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBHelper.SQLKinetic
+{
+    public class KineticSNTran
+    {
+        private AccessSQL dbAccess = new AccessSQL();
+
+        public KineticSNTran()
+        {
+            dbAccess.SetConnStr(DBConnectionStr.SQLEpicorConnStr());
+        }
+
+        public DataTable GetAMCPartNumber(string pn)
+        {
+            string sqlCmd = string.Format("select top 1 TranType, SNStatus from [dbo].SNTran where SerialNumber = '{0}' order by TranNum desc", pn);
+
+            dbAccess.SetQueryCmd(sqlCmd);
+
+            DataTable dt = dbAccess.LoadDbData();
+
+            return dt;
+        }
+
+    } // class
+}
