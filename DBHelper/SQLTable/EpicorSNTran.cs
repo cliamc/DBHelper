bb1ef06f49d7b74f8b44f355d9e03822471a377d
cli <cli@DE-D-CLI>
2024-02-12 08:41:44 -0800

    Create EpicorSNTran.cs

---
+++ DBHelper/SQLTable/EpicorSNTran.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBHelper.SQLTable
+{
+
+    public class EpicorSNTran
+    {
+        private AccessSQL dbAccess = new AccessSQL();
+
+        public EpicorSNTran()
+        {
+            dbAccess.SetConnStr(DBConnectionStr.SQLEpicorConnStr());
+        }
+
+        public DataTable GetScrappedStatus(string sn)
+        {
+            string sqlCmd = string.Format("select top 1 TranType, SNStatus from [dbo].SNTran where SerialNumber = '{0}' order by TranNum desc", sn);
+
+            dbAccess.SetQueryCmd(sqlCmd);
+
+            DataTable dt = dbAccess.LoadDbData();
+
+            return dt;
+        }
+
+    } // class
+}
