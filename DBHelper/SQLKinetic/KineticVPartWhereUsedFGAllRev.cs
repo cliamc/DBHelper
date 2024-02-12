0916b1aa6c951f7c06ccacdd51101a411fc04a0c
cli <cli@DE-D-CLI>
2024-02-12 08:41:38 -0800

    Create KineticVPartWhereUsedFGAllRev.cs

---
+++ DBHelper/SQLKinetic/KineticVPartWhereUsedFGAllRev.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBHelper.SQLKinetic
+{
+    public struct MaterialAndQty
+    {
+        public string material;
+        public int qty;
+    }
+
+    public class KineticVPartWhereUsedFGAllRev
+    {
+        private AccessSQL dbAccess = new AccessSQL();
+
+        private string partNum = "";
+        private string partRev = "";
+
+        //public List<string> AllParts = new List<string>();
+        private List<string> uniqueParts = new List<string>();
+
+        public List<MaterialAndQty> materialList = new List<MaterialAndQty>();
+
+        public KineticVPartWhereUsedFGAllRev(string pn, string ver)
+        {
+            this.partNum = pn;
+            this.partRev = ver;
+
+            dbAccess.SetConnStr(DBConnectionStr.SQLKineticConnStr());
+
+            SetAllParts();
+        }
+
+        private void SetAllParts()
+        {
+            string sqlCmd = string.Format("select * from dbo.v_PartWhereUsedFG_AllRev where PartNum = '{0}' and RevisionNum = '{1}' order by MtlPartNum", this.partNum, this.partRev);
+            dbAccess.SetQueryCmd(sqlCmd);
+            DataTable dt = dbAccess.ReadDbData();
+
+            if (dt.Rows.Count > 0)
+            {
+                foreach (DataRow dRow in dt.Rows)
+                {
+                    string tQty = (string)dRow["QtyPer"].ToString();
+                    int pos = tQty.IndexOf(".");
+                    string intPortion = tQty.Substring(0, pos);
+                    int tfloat = Convert.ToInt32(intPortion);
+                    string tmp = (string)dRow["MtlPartNum"];
+
+                    MaterialAndQty maq = new MaterialAndQty();
+                    maq.material = tmp;
+                    maq.qty = tfloat;
+                    materialList.Add(maq);
+
+                    uniqueParts.Add(tmp);
+
+                    //for (int i = 1; i <= tfloat; i++)
+                    //{
+                    //    AllParts.Add(tmp);
+                    //}
+                }
+            }
+        }
+
+        public List<string> GetUniqueParts()
+        {
+            return this.uniqueParts;
+        }
+
+    } // class
+}
